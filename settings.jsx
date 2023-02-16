238819c4c3ca6f8a4215fdc560a6bd22ed2b6f5c
Evan Hunzinger <evan@hunz.dev>
2023-02-16 17:26:49 -0600

    Adjust default setting for multiple templates

--- settings.jsx
+++ settings.jsx
@@ -30,7 +30,7 @@ var specified_template = null;
 // var specified_template = PhyrexianTemplate; // REGULAR - Secret lair praetor showcase
 // var specified_template = CrimsonFangTemplate; // REGULAR - Crimson Vow showcase
 // var specified_template = DoubleFeatureTemplate; // NORMAL - Mid/Vow double feature
-// var specified_template = [NormalTemplate, SilvanExtendedTemplate]; // Do multiple templates at a time
+// var specified_template = [NormalTemplate, NormalExtendedTemplate]; // Do multiple templates at a time
 
 // Specify whether to end the script when the card is finished being formatted (for manual intervention)
 var exit_early = false;
