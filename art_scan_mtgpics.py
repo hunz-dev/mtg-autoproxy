676663e103a5f13085fe8e83e80574d330b6bf9b
Evan Hunzinger <evan@hunz.dev>
2023-02-26 23:52:25 -0600

    Add basic dataclass for Scryfall card object

--- art_scan_mtgpics.py
+++ art_scan_mtgpics.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 import requests
 # from bs4 import BeautifulSoup
 
@@ -10,8 +11,52 @@ MTGPICS_URL = "https://mtgpics.com"
 SCRYFALL_URL = "https://api.scryfall.com/cards/search"
 
 
+@dataclass
 class ScryfallCard:
-    pass
+    # Pull needed attributes from here: https://scryfall.com/docs/api/cards
+    artist: str
+    collector_number: str
+    full_art: bool
+    id: str
+    layout: str
+    name: str
+    oracle_id: str
+    rarity: str
+    scryfall_uri: str
+    set: str
+    set_name: str
+
+    ATTRIBUTES = [
+        "artist",
+        "collector_number",
+        "full_art",
+        "id",
+        "layout",
+        "name",
+        "oracle_id",
+        "rarity",
+        "scryfall_uri",
+        "set",
+        "set_name",
+    ]
+
+    def __init__(self, _json=None):
+        if _json is None:
+            raise ValueError("JSON-like dictionary from Scryfall API is required")
+        [setattr(self, attribute, _json[attribute]) for attribute in ScryfallCard.ATTRIBUTES]
+
+
+    def __str__(self):
+        return f"{self.name} ({self.artist}) [{self.set.upper()}]"
+
+
+def get_scryfall_card(card_name, set_code) -> ScryfallCard:
+    print(f"Searching Scryfall: \"{card_name} [{set_code}]\"... ", end="", flush=True)
+    params = dict(fuzzy=card_name, set=set_code)
+    response = requests.get(SCRYFALL_URL, params=params)
+    card = ScryfallCard(response.json())
+    print(f"Found: \"{card}\"")
+    return card
 
 
 def get_scryfall_card(query):
