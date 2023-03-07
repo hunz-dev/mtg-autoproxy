f3067013705be5cece4945f7624f7272c2ca9924
Evan Hunzinger <evan@hunz.dev>
2023-03-07 12:54:41 -0600

    Use default MDFC templates

--- scripts/plugins/Silvan/layouts.jsx
+++ scripts/plugins/Silvan/layouts.jsx
@@ -4,5 +4,5 @@ class_template_map[mdfc_front_class].other.push(SilvanMDFCFrontTemplate);
 class_template_map[mdfc_back_class].other.push(SilvanMDFCBackTemplate);
 
 // Uncomment these to make them your default MDFC templates
-class_template_map[mdfc_front_class].default_ = SilvanMDFCFrontTemplate;
-class_template_map[mdfc_back_class].default_ = SilvanMDFCBackTemplate;
+// class_template_map[mdfc_front_class].default_ = SilvanMDFCFrontTemplate;
+// class_template_map[mdfc_back_class].default_ = SilvanMDFCBackTemplate;
