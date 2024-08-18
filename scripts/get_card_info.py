9ccbcfb8829fad1fac994130ede56f07c1241636
Evan Hunzinger <evan@hunz.dev>
2024-08-18 11:32:54 -0500

    Move main behavior to function for easier debugging and add to scratchpad

--- scripts/get_card_info.py
+++ scripts/get_card_info.py
@@ -20,10 +20,7 @@ def add_meld_info(card_json):
 
     return card_json
 
-
-if __name__ == "__main__":
-    card_name = sys.argv[1]
-    # Use Scryfall to search for this card
+def main(card_name: str):
     card = None
 
     # If the card specifies which set to retrieve the scan from, do that
@@ -59,3 +56,7 @@ if __name__ == "__main__":
 
     print(" and done!", flush=True)
     time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
