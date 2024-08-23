7b4f55e2ede6bdcd035f235a5b11db68cb7ff9bb
Evan Hunzinger <evan@hunz.dev>
2024-08-23 08:22:38 -0500

    Cleaned up and simplified art scan entrypoint

--- art_scan.py
+++ art_scan.py
@@ -11,18 +11,11 @@ from lib.common import get_rate_limit_wait
 
 
 DEEPAI_BASE_URL = "https://api.deepai.org/api"
-
-# DeepAI API key to use for upscaling models, should be placed in `/.env` file
 DEEPAI_KEY = os.environ['DEEPAI_KEY'].replace("\r", "")
+DEEPAI_MODEL_NAME = "torch-srgan"
 
 
-# Specify a list of queries for Scryfall
-queries = [
-    # ex. "arbor elf set:wwk",
-    # "",
-]
-
-def save_deepai_image(card: ScryfallCard, model_name="waifu2x") -> None:
+def save_deepai_image(card: ScryfallCard, model_name=DEEPAI_MODEL_NAME) -> None:
     """Save a DeepAI generated image upscaled from Scryfall image.
 
     Args:
@@ -91,22 +84,19 @@ def process_query(query: str, force_scryfall=False, skip_mtgpics=False, skip_scr
     # Fetch HTML from MTGPICS with card info and save images
     results = list()
     if not skip_mtgpics:
-        card_name, gamerid = mtgpics_helpers.get_gamerid(query=query)
-        for version in mtgpics_helpers.find_all_art_versions(card_name, gamerid):
-            results.append(mtgpics_helpers.save_image(version))
-
-        # If nothing was found using main approach, use alternate
-        if not any(results):
-            for card in cards:
-                results.append(mtgpics_helpers.save_image_alt(card))
+        result = mtgpics_helpers.get_gamerid(query=query)
+        if not result:
+            print("No gamerid found for query...")
+        else:
+            for version in mtgpics_helpers.find_all_art_versions(*result):
+                results.append(mtgpics_helpers.save_image(version))
 
     # If nothing is found, use Scryfall art w/ AI upscale
     if not skip_scryfall and (not any(results) or force_scryfall):
         for card in cards:
-            save_deepai_image(card, model_name="torch-srgan")
+            save_deepai_image(card)
 
 
 if __name__ == "__main__":
-    queries = queries if len(queries) > 0 else read_stdin()
     # Parse out asterisk to force scryfall image search
-    [process_query(q.replace("*", ""), force_scryfall=("*" in q)) for q in queries]
+    [process_query(q.replace("*", ""), force_scryfall=("*" in q)) for q in read_stdin()]
