b3dab02a01dde9d342e81fd65f2219027bc7db03
Evan Hunzinger <evan@hunz.dev>
2024-08-25 12:36:22 -0500

    Add Inventory class

--- lib/classes/inventory.py
+++ lib/classes/inventory.py
@@ -1,4 +1,5 @@
-from typing import List, Union
+from typing import List, Optional, Union
+
 
 class InventoryCard:
     """Represent attributes of a card for an inventory.
@@ -18,33 +19,11 @@ class InventoryCard:
     order_count: int
     type_: str
 
-    # Map column numbers from spreadsheet
-    COLUMN_MAP = {
-        "name": 0,
-        "type_": 1,
-        "modified": 2,
-        "counts": (3, -3),
-        "on_hand": -2,
-        "order_count": -1,
-    }
-
-    VALID_TYPES = [
-        "White",
-        "Blue",
-        "Black",
-        "Red",
-        "Green",
-        "Land",
-        "Multi",
-        "Colorless",
-        "Token",
-    ]
-
     def __init__(self,
             name: str, type_: str, modified: str, counts: List[Union[str, int]],
             on_hand: Union[str, int], order_count: Union[str, int]):
-        if type_ not in InventoryCard.VALID_TYPES:
-            raise ValueError(f"Type must be one of: {InventoryCard.VALID_TYPES}")
+        if type_ not in Inventory.VALID_TYPES:
+            raise ValueError(f"Type must be one of: {Inventory.VALID_TYPES}")
 
         self.name = name
         self.modified = modified  # TODO: Convert to datetime?
@@ -57,12 +36,12 @@ class InventoryCard:
     def from_row(cls, row: str):
         try:
             return cls(
-                name=row[InventoryCard.COLUMN_MAP['name']],
-                type_=row[InventoryCard.COLUMN_MAP['type_']],
-                modified=row[InventoryCard.COLUMN_MAP['modified']],
-                counts=row[InventoryCard.COLUMN_MAP['counts'][0]:InventoryCard.COLUMN_MAP['counts'][1]],
-                on_hand=row[InventoryCard.COLUMN_MAP['order_count']],
-                order_count=row[InventoryCard.COLUMN_MAP['order_count']])
+                name=row[Inventory.COLUMN_MAP['name']],
+                type_=row[Inventory.COLUMN_MAP['type_']],
+                modified=row[Inventory.COLUMN_MAP['modified']],
+                counts=row[Inventory.COLUMN_MAP['counts'][0]:Inventory.COLUMN_MAP['counts'][1]],
+                on_hand=row[Inventory.COLUMN_MAP['order_count']],
+                order_count=row[Inventory.COLUMN_MAP['order_count']])
         except IndexError as e:
             print(f"Error parsing: {row}")
             raise e
@@ -71,7 +50,7 @@ class InventoryCard:
             raise e
 
     def as_separated_value(self, separator=" "):
-        output = {k: getattr(self, k) for k in InventoryCard.COLUMN_MAP.keys()}
+        output = {k: getattr(self, k) for k in Inventory.COLUMN_MAP.keys()}
         output["counts"] = separator.join([str(c) for c in output["counts"]])
         return separator.join([str(v) for v in output.values()])
     
@@ -88,3 +67,46 @@ class InventoryCard:
 
     def __str__(self):
         return self.csv
+
+
+class Inventory:
+    cards: List[InventoryCard]
+    sheet_id: Optional[str]
+    users: List[str]
+
+    # Map column numbers from spreadsheet
+    COLUMN_MAP = {
+        "name": 0,
+        "type_": 1,
+        "modified": 2,
+        "counts": (3, -3),
+        "on_hand": -2,
+        "order_count": -1,
+    }
+    HEADER_ROW = 4  # Ignore calculated fields in rows 1-3
+    VALID_TYPES = [
+        "White",
+        "Blue",
+        "Black",
+        "Red",
+        "Green",
+        "Land",
+        "Multi",
+        "Colorless",
+        "Token",
+    ]
+
+    def __init__(self, cards: List[InventoryCard], users: List[str], sheet_id: str = None):
+        self.cards = cards
+        self.sheet_id = sheet_id
+        self.users = users
+
+    @classmethod
+    def from_csv(cls, input: List[List[str]]):
+        header = input[Inventory.HEADER_ROW - 1]
+        users = header[Inventory.COLUMN_MAP["counts"][0]:Inventory.COLUMN_MAP["counts"][1]]
+        cards = [InventoryCard.from_row(r) for r in input[Inventory.HEADER_ROW:]]
+        return Inventory(cards, users)
+
+    def __str__(self):
+        return f"Inventory: {len(self.cards)} cards for {len(self.users)} users ({', '.join(self.users)})"
