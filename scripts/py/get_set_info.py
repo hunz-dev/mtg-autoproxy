b6bec0492099b577ca38041c2f2a7a6c7e82f681
Evan Hunzinger <evan@hunz.dev>
2024-11-07 05:54:51 -0600

    Clean up set code fetch

--- scripts/py/get_set_info.py
+++ scripts/py/get_set_info.py
@@ -1,33 +1,22 @@
-import time
 import sys
-import json
-from urllib import request, parse, error
+import simplejson as json
+from lib.helpers import scryfall_helpers
 
 
 # TODO: Clean up
-CARD_OUTPUT_PATH = "C:\\Users\\evanh\\Code\\mtg-autoproxy\\scripts\\jsx\\set.json"
+SET_OUTPUT_PATH = "scripts/jsx/set.json"
+
+
+def main(set_code: str):
+    print(f"Searching Scryfall for set: {set_code}...", end="", flush=True)
+    set_ = scryfall_helpers.get_set_info(set_code)
+
+    print("Saving JSON...", end="", flush=True)
+    with open(SET_OUTPUT_PATH, 'w') as f:
+        f.write(json.dumps(set_, indent=4, sort_keys=True))
+
+    print("Done!")
 
 
 if __name__ == "__main__":
-    set_code = sys.argv[1]
-    # Use Scryfall to search for this card
-    mtg_set = None
-
-    # If the card specifies which set to retrieve the scan from, do that
-    try:
-        print(f"Searching Scryfall for: Set: {set_code}...", end="", flush=True)
-        mtg_set = request.urlopen(
-            f"https://api.scryfall.com/sets/{parse.quote(set_code)}"
-        ).read()
-    except error.HTTPError:
-        print("\nCouldn't retrieve set information. Probably no big deal!")
-        time.sleep(1)
-
-    print(" and done! Saving JSON...", end="", flush=True)
-
-    json_dump = json.dumps(json.loads(mtg_set))
-    with open(CARD_OUTPUT_PATH, 'w') as f:
-        json.dump(json_dump, f)
-
-    print(" and done!", flush=True)
-    time.sleep(0.1)
+    main(sys.argv[1])
